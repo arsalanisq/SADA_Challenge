7fae400b347245dcd6624aa48e8def318d9aa428
arsalanisq <91771973+arsalanisq@users.noreply.github.com>
2021-10-01 23:40:14 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,27 +4,26 @@ echo 25500
 echo 2. Remove all rows with 'profit' that is not numerical value. Then print out how many rows of data are left
 echo 25131
 
-echo 3. Print the top 20 rows with the highest profit values.
-echo Year	Rank	Company	Revenue (in millions)
-echo Profit (in millions)				
-echo 25330	2005	2	Exxon Mobil	270772
-echo 22071	1999	2	Ford Motor	144416
-echo 21510	2004	2	Exxon Mobil	213199
-echo 17853	2004	8	Citigroup	94713
-echo 17720	2001	1	Exxon Mobil	210392
-echo 17046	2005	8	Citigroup	108276
-echo 16593	2005	5	General Electric	152363
-echo 15320	2002	2	Exxon Mobil	191581
-echo 15276	2003	6	Citigroup	100789
-echo 15002	2004	5	General Electric	134187
-echo 14143	2005	18	Bank of America Corp.	63324
-echo 14126	2002	7	Citigroup	112022
-echo 14118	2003	5	General Electric	131698
-echo 13684	2002	6	General Electric	125913
-echo 13519	2001	6	Citigroup	111826
-echo 13328	2005	6	ChevronTexaco	147967
-echo 12735	2001	5	General Electric	129853
-echo 11797	2001	10	Verizon Communications	64707
-echo 11460	2003	3	Exxon Mobil	182466
-echo 11361	2005	24	Pfizer	52921
+echo 3. Print the top twenty rows with the highest profit values.
+echo Profit (in millions) Year	Rank	Company	Revenue (in millions)			
+echo 25330	              2005	2	Exxon Mobil	270772
+echo 22071	              1999	2	Ford Motor	144416
+echo 21510	              2004	2	Exxon Mobil	213199
+echo 17853	              2004	8	Citigroup	94713
+echo 17720	              2001	1	Exxon Mobil	210392
+echo 17046	              2005	8	Citigroup	108276
+echo 16593	              2005	5	General Electric	152363
+echo 15320	              2002	2	Exxon Mobil	191581
+echo 15276	              2003	6	Citigroup	100789
+echo 15002	              2004	5	General Electric	134187
+echo 14143	              2005	18	Bank of America Corp.	63324
+echo 14126	              2002	7	Citigroup	112022
+echo 14118	              2003	5	General Electric	131698
+echo 13684	              2002	6	General Electric	125913
+echo 13519	              2001	6	Citigroup	111826
+echo 13328	              2005	6	ChevronTexaco	147967
+echo 12735	              2001	5	General Electric	129853
+echo 11797	              2001	10	Verizon Communications	64707
+echo 11460	              2003	3	Exxon Mobil	182466
+echo 11361	              2005	24	Pfizer	52921
 
