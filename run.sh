d6ed2159d4ed552dc635b4117a21a8a5f186e5eb
arsalanisq <91771973+arsalanisq@users.noreply.github.com>
2021-10-01 23:49:34 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ echo 2. Remove all rows with 'profit' that is not numerical value. Then print ou
 echo 25131
 
 echo 3. Print the top twenty rows with the highest profit values		
-echo Profit 	 Year	Rank	Company	Revenue (in millions)
+echo Profit,Year,Rank,Company,Revenue
