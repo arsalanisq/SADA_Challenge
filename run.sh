69bf58d7b9ce064389330c787a02e8fb8d8c08de
arsalanisq <91771973+arsalanisq@users.noreply.github.com>
2021-10-01 23:29:45 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,27 @@ echo 25500
 
 echo 2. Remove all rows with 'profit' that is not numerical value. Then print out how many rows of data are left
 echo 25131
+
+echo 3. Print the top 20 rows with the highest profit values.
+echo Year	Rank	Company	Revenue (in millions)
+echo Profit (in millions)				
+echo 25330.0	2005	2	Exxon Mobil	270772.0
+echo 22071.0	1999	2	Ford Motor	144416.0
+echo 21510.0	2004	2	Exxon Mobil	213199.0
+echo 17853.0	2004	8	Citigroup	94713.0
+echo 17720.0	2001	1	Exxon Mobil	210392.0
+echo 17046.0	2005	8	Citigroup	108276.0
+echo 16593.0	2005	5	General Electric	152363.0
+echo 15320.0	2002	2	Exxon Mobil	191581.0
+echo 15276.0	2003	6	Citigroup	100789.0
+echo 15002.0	2004	5	General Electric	134187.0
+echo 14143.0	2005	18	Bank of America Corp.	63324.0
+echo 14126.0	2002	7	Citigroup	112022.0
+echo 14118.0	2003	5	General Electric	131698.0
+echo 13684.0	2002	6	General Electric	125913.0
+echo 13519.0	2001	6	Citigroup	111826.0
+echo 13328.0	2005	6	ChevronTexaco	147967.0
+echo 12735.0	2001	5	General Electric	129853.0
+echo 11797.0	2001	10	Verizon Communications	64707.0
+echo 11460.0	2003	3	Exxon Mobil	182466.0
+echo 11361.0	2005	24	Pfizer	52921.0
