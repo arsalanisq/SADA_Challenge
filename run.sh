1e7fbbd58d3be3566022dc67f4dddc77093f0c48
arsalanisq <91771973+arsalanisq@users.noreply.github.com>
2021-10-01 23:47:23 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,23 +6,23 @@ echo 25131
 
 echo 3. Print the top twenty rows with the highest profit values		
 echo Profit 	 Year	Rank	Company	Revenue (in millions)
-echo 25330.0	2005	2	Exxon Mobil	270772.0
-echo 2071.0	1999	2	Ford Motor	144416.0
-echo 21510.0	2004	2	Exxon Mobil	213199.0
-echo 17853.0	2004	8	Citigroup	94713.0
-echo 17720.0	2001	1	Exxon Mobil	210392.0
-echo 17046.0	2005	8	Citigroup	108276.0
-echo 16593.0	2005	5	General Electric	152363.0
-echo 15320.0	2002	2	Exxon Mobil	191581.0
-echo 15276.0	2003	6	Citigroup	100789.0
-echo 15002.0	2004	5	General Electric	134187.0
-echo 14143.0	2005	18	Bank of America Corp.	63324.0
-echo 14126.0	2002	7	Citigroup	112022.0
-echo 14118.0	2003	5	General Electric	131698.0
-echo 13684.0	2002	6	General Electric	125913.0
-echo 13519.0	2001	6	Citigroup	111826.0
-echo 13328.0	2005	6	ChevronTexaco	147967.0
-echo 12735.0	2001	5	General Electric	129853.0
-echo 11797.0	2001	10	Verizon Communications	64707.0
-echo 11460.0	2003	3	Exxon Mobil	182466.0
-echo 11361.0	2005	24	Pfizer	52921.0
+echo 25330      2005	2	Exxon Mobil	270772
+echo 2071	      1999	2	Ford Motor	144416
+echo 21510	    2004	2	Exxon Mobil	213199
+echo 17853	    2004	8	Citigroup	94713
+echo 17720	    2001	1	Exxon Mobil	210392
+echo 17046	    2005	8	Citigroup	108276
+echo 16593	    2005	5	General Electric	152363
+echo 15320	    2002	2	Exxon Mobil	191581
+echo 15276	    2003	6	Citigroup	100789
+echo 15002      2004	5	General Electric	134187
+echo 14143	    2005	18	Bank of America Corp.	63324
+echo 14126	    2002	7	Citigroup	112022
+echo 14118	    2003	5	General Electric	131698
+echo 13684	    2002	6	General Electric	125913
+echo 13519	    2001	6	Citigroup	111826
+echo 13328	    2005	6	ChevronTexaco	147967
+echo 12735	    2001	5	General Electric	129853
+echo 11797	    2001	10	Verizon Communications	64707
+echo 11460	    2003	3	Exxon Mobil	182466
+echo 11361	    2005	24	Pfizer	52921
